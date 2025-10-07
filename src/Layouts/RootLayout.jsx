3f5a5ad368100bf0ac982702dd018b00103a4794
Touhid Tamim <touhidtamim1@gmail.com>
2025-10-07 12:31:06 +0600

    added a custom scroll to top component & made data theme to light

--- src/Layouts/RootLayout.jsx
+++ src/Layouts/RootLayout.jsx
@@ -2,12 +2,16 @@ import React from "react";
 import { Outlet } from "react-router-dom";
 import Navbar from "../Components/Navbar/Navbar";
 import Footer from "../Components/Footer/Footer";
+import ScrollToTop from "./../Components/Shared/ScrollToTop";
 
 const RootLayout = () => {
   return (
     <>
       {/* Navbar */}
       <Navbar />
+
+      {/* Added a custom scroll to top component to make sure better user experience  */}
+      <ScrollToTop />
 
       <Outlet />
 
