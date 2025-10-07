e3575963613e08465282aa5e0d297a13cefa1565
Touhid Tamim <touhidtamim1@gmail.com>
2025-10-07 12:25:37 +0600

    fixed root layouts bugs

--- src/Layouts/RootLayout.jsx
+++ src/Layouts/RootLayout.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Outlet } from "react-router";
+import { Outlet } from "react-router-dom";
 import Navbar from "../Components/Navbar/Navbar";
 import Footer from "../Components/Footer/Footer";
 
