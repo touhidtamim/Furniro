3429521909d61086a4507ca1339290c966659c36
Touhid Tamim <touhidtamim1@gmail.com>
2025-10-31 21:59:43 +0600

    updated banner

--- src/Components/Home/Banner.jsx
+++ src/Components/Home/Banner.jsx
@@ -15,19 +15,19 @@ const Banner = () => {
       <div className="absolute inset-0 bg-black/10"></div>
 
       {/* Content Box */}
-      <div className="relative max-w-md bg-white/90 backdrop-blur-sm text-gray-900 p-10 rounded-2xl shadow-xl mr-36">
-        <p className="text-sm uppercase tracking-[0.2em] text-gray-500 font-semibold">
+      <div className="relative max-w-md bg-[#ebe1c8] backdrop-blur-sm text-gray-900 p-10 rounded-2xl shadow-xl mr-36">
+        <p className="text-sm uppercase tracking-[0.2em] text-black font-semibold">
           New Arrival
         </p>
-        <h3 className="text-2xl font-extrabold mt-2 leading-tight">
+        <h3 className="text-2xl text-[#B88E2F] font-extrabold mt-2 leading-tight">
           Redefine Your Space <br /> with Modern Elegance
         </h3>
-        <p className="mt-4 text-gray-700 leading-relaxed">
+        <p className="mt-4 text-black leading-relaxed">
           Experience the perfect balance of comfort and style. Explore our
           latest Scandinavian-inspired furniture that transforms your home into
           a timeless masterpiece.
         </p>
-        <button className="mt-6 px-7 py-3 bg-black text-white font-medium rounded-lg hover:bg-gray-800 transition">
+        <button className="mt-6 px-7 py-3 bg-[#B88E2F] text-white font-medium rounded-lg hover:bg-gray-800 transition">
           Shop Now
         </button>
       </div>
