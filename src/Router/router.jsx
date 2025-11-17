5180ba3fb9f675fe35bb9cb5e7a77b9870fa8467
Touhid Tamim <touhidtamim1@gmail.com>
2025-11-17 22:37:41 +0600

    updated routing setup

--- src/Router/router.jsx
+++ src/Router/router.jsx
@@ -1,6 +1,8 @@
 import { createBrowserRouter } from "react-router-dom";
 import RootLayout from "../Layouts/RootLayout";
 import Home from "../Pages/Home/Home";
+import About from "../Pages/About/About";
+import Shop from "../Pages/Shop/Shop";
 
 const router = createBrowserRouter([
   {
@@ -10,6 +12,14 @@ const router = createBrowserRouter([
       {
         index: true,
         element: <Home />,
+      },
+      {
+        path: "about",
+        element: <About />,
+      },
+      {
+        path: "shop",
+        element: <Shop />,
       },
     ],
   },
