626b39fa172fcfa0dae8c31e0740ac6393c6f32f
Touhid Tamim <touhidtamim1@gmail.com>
2025-12-28 20:49:31 +0600

    updated home page

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Banner from "../../Components/Home/Banner";
 import BrowseTheRange from "../../Components/Home/BrowseTheRange";
+import CTA from "../../Components/Home/CTA";
 
 const Home = () => {
   return (
     <div>
       <Banner />
       <BrowseTheRange />
+      <CTA />
     </div>
   );
 };
