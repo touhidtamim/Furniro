bc8b11ad334fc5109925331f82819a8a7486659f
Touhid Tamim <touhidtamim1@gmail.com>
2025-12-29 21:09:08 +0600

    updated home and add testimonial section

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Banner from "../../Components/Home/Banner";
 import BrowseTheRange from "../../Components/Home/BrowseTheRange";
 import CTA from "../../Components/Home/CTA";
+import Testimonial from "./../../Components/Home/Testiminial";
 
 const Home = () => {
   return (
@@ -9,6 +10,7 @@ const Home = () => {
       <Banner />
       <BrowseTheRange />
       <CTA />
+      <Testimonial />
     </div>
   );
 };
