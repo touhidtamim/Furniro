cecfc4f38d5c6cfe20e3284435c148a4d6bd925d
Touhid Tamim <touhidtamim1@gmail.com>
2026-01-05 21:43:06 +0600

    updated shop page with items component

--- src/Pages/Shop/Shop.jsx
+++ src/Pages/Shop/Shop.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import SecondaryHero from "./../../Components/Shared/seconderyHero";
+import Items from "./Items";
 
 const Shop = () => {
   return (
     <>
       {/* Secondery HeroImg */}
       <SecondaryHero />
+
+      {/* Shoping Items */}
+      <Items />
     </>
   );
 };
