70c9b6b5f5dec84f3d75451e4b67bafa2bd53f19
Touhid Tamim <touhidtamim1@gmail.com>
2026-01-28 13:00:49 +0600

    updated btn in login page

--- src/Pages/login/Login.jsx
+++ src/Pages/login/Login.jsx
@@ -50,6 +50,12 @@ const Login = () => {
           </div>
 
           {/* Button */}
+          <button
+            type="submit"
+            className="w-full py-2.5 bg-sky-600 hover:bg-sky-700 text-white font-semibold rounded-lg transition"
+          >
+            Login
+          </button>
         </form>
 
         {/* Footer */}
