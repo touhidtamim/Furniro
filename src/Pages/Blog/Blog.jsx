9b57fb7e03a8484e7c9af5e503bdab5c6819894a
Touhid Tamim <touhidtamim1@gmail.com>
2026-02-18 14:31:00 +0600

    working on blogs

--- src/Pages/Blog/Blog.jsx
+++ src/Pages/Blog/Blog.jsx
@@ -1,8 +1,12 @@
 import React from "react";
+import SecondaryHero from "../../Components/Shared/seconderyHero";
 
 const Blog = () => {
   return (
     <div>
+      {/* Secondery hero  */}
+      <SecondaryHero />
+
       <h1>This is blog page</h1>
     </div>
   );
